63d4d1fe43f31c4858d4d5f34158e264025b16a0
shunyasan <shunya410.coo@icloud.com>
2020-12-06 23:21:57 +0900

    loginセッションメッセージの追加

--- src/app/Http/Middleware/RedirectIfAuthenticated.php
+++ src/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,7 @@ class RedirectIfAuthenticated
     {
         if (Auth::guard($guard)->check()) {
 
-            return redirect(route('favorite'));
+            return redirect(route('top'));
         }
 
         return $next($request);
